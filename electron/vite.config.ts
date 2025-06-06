03b98981ea3ed08089ab587ad87c2444cb8e39e9
Topabaem <87803226+Topabaem05@users.noreply.github.com>
2025-06-06 23:25:07 +0900

    Replace web build with Electron

--- electron/vite.config.ts
+++ electron/vite.config.ts
@@ -6,12 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: 'all'
+    port: 3000
   },
   preview: {
     host: '0.0.0.0',
-    port: 4173,
-    allowedHosts: 'all'
+    port: 4173
   }
 })
